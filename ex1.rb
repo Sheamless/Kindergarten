ae0474945aff31582e8dcb9048c7d42c0654993a
Sheamless <shea.wolcott.daniel@gmail.com>
2018-03-15 17:56:53 -0400

    Exersize 1

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+      "Hello world!"
+ "Hello Again"
+#puts"I like typing this."
+#{}"This is fun"
+puts "Hello"#{}"Yay! Printing."
+"I'd much rather you 'not'."
+'I "said" do not touch this.'
+ "Hello Chris"
+ #End of ex.1 - messed with hashtags - blank lines
